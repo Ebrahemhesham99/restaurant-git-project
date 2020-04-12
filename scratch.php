938ed54794793dcc07357ad517198e9a916d49a8
Ebrahemhesham99 <63474630+Ebrahemhesham99@users.noreply.github.com>
2020-04-13 00:32:53 +0200

    Add files via upload

---
+++ scratch.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>fafoo</title>
+    <!----------------------------------------------------------------------------------------------------------------------------------------->
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!------------------------------------------------------------------css files----------------------------------------------------------------------->
+        <link href="css/restfoo.css" rel="stylesheet">
+    <!------------------------------------------------------------------google fonts----------------------------------------------------------------------->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="css/fontawesome-free-5.9.0-web/css/fontawesome.min.css">
+               
+    <!------------------------------------------------------------------bootstrap----------------------------------------------------------------------->
+    <link rel="stylesheet" type="text/css" href="css/bootstrap/css/bootstrap.min.css"/>
+    </head>
+    <body >
+    <div class="banner">
+        <nav class="nav">
+            <ul>
+                <li><a href="">home</a></li>
+                <li><a href="">features</a></li>
+                <li><a href="">team</a></li>
+                <li><a href="">work</a></li>
+                <li><a href="">pricining</a></li>
+                <li><a href="">client</a></li>
+                <li><a href="">blog</a></li>
+                <li><a href="">contact</a></li>
+            </ul>
+        </nav>
+</body>
